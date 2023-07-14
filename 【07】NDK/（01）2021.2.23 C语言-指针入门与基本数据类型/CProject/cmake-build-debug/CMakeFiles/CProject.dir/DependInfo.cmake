8f762db5de9b931aba8791a7166feffbfc1cca8d
lyyyd <David.Jackson.Lyd@gmail.com>
2023-07-14 20:37:07 +0800

    （01）2021.2.23 C语言-指针入门与基本数据类型
    （01）2021.2.23 C语言-指针入门与基本数据类型

---
+++ 【07】NDK/（01）2021.2.23 C语言-指针入门与基本数据类型/CProject/cmake-build-debug/CMakeFiles/CProject.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/T1.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/cmake-build-debug/CMakeFiles/CProject.dir/T1.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/T2.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/cmake-build-debug/CMakeFiles/CProject.dir/T2.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/T3.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/cmake-build-debug/CMakeFiles/CProject.dir/T3.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/T4.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/cmake-build-debug/CMakeFiles/CProject.dir/T4.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/T5.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/cmake-build-debug/CMakeFiles/CProject.dir/T5.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/T6.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/cmake-build-debug/CMakeFiles/CProject.dir/T6.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/T7.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/cmake-build-debug/CMakeFiles/CProject.dir/T7.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/T8.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk01/project/CProject/cmake-build-debug/CMakeFiles/CProject.dir/T8.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 【07】NDK/（01）2021.2.23 C语言-指针入门与基本数据类型/CProject/cmake-build-debug/CMakeFiles/CProject.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CProject.dir/T1.c.obj"
+  "CMakeFiles/CProject.dir/T2.c.obj"
+  "CMakeFiles/CProject.dir/T3.c.obj"
+  "CMakeFiles/CProject.dir/T4.c.obj"
+  "CMakeFiles/CProject.dir/T5.c.obj"
+  "CMakeFiles/CProject.dir/T6.c.obj"
+  "CMakeFiles/CProject.dir/T7.c.obj"
+  "CMakeFiles/CProject.dir/T8.c.obj"
+  "CProject.exe"
+  "CProject.exe.manifest"
+  "CProject.lib"
+  "CProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
