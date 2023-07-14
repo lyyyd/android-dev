852f95290e8ec99fbdb518a2ed9c79b07cfbcbb0
lyyyd <David.Jackson.Lyd@gmail.com>
2023-07-14 20:38:34 +0800

    （02）2021.2.25 函数指针与指针运算（NDK第二节课）
    （02）2021.2.25 函数指针与指针运算（NDK第二节课）

---
+++ 【07】NDK/（02）2021.2.25 函数指针与指针运算（NDK第二节课）/CProject/cmake-build-debug/CMakeFiles/CProject.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CProject.dir/T1.c.obj"
+  "CMakeFiles/CProject.dir/T2.c.obj"
+  "CMakeFiles/CProject.dir/T3.c.obj"
+  "CMakeFiles/CProject.dir/T4.c.obj"
+  "CMakeFiles/CProject.dir/T5.c.obj"
+  "CMakeFiles/CProject.dir/T6.c.obj"
+  "CMakeFiles/CProject.dir/T7.c.obj"
+  "CMakeFiles/CProject.dir/T8.c.obj"
+  "CMakeFiles/CProject.dir/T9.c.obj"
+  "CProject.exe"
+  "CProject.exe.manifest"
+  "CProject.pdb"
+  "libCProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
