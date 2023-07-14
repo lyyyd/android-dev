6ea3f80054e43d9bcf222442b191dcd05869246f
lyyyd <David.Jackson.Lyd@gmail.com>
2023-07-14 21:05:47 +0800

    （18）2021.4.11 JNI技术之JNI详讲交互与预处理器（NDK第十八节课）
    （18）2021.4.11 JNI技术之JNI详讲交互与预处理器（NDK第十八节课）

---
+++ 【07】NDK/（18）2021.4.11 JNI技术之JNI详讲交互与预处理器（NDK第十八节课）/ClionCPPProject/cmake-build-debug/CMakeFiles/ClionCPPProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClionCPPProject.dir/T5.cpp.o"
+  "ClionCPPProject.exe"
+  "ClionCPPProject.pdb"
+  "libClionCPPProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClionCPPProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
