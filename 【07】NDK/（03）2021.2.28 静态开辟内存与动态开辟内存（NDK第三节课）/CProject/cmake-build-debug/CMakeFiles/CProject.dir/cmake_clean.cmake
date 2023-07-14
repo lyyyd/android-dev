b1352cf70af5b2808575466506549846e736d668
lyyyd <David.Jackson.Lyd@gmail.com>
2023-07-14 20:39:51 +0800

    （03）2021.2.28 静态开辟内存与动态开辟内存（NDK第三节课）
    （03）2021.2.28 静态开辟内存与动态开辟内存（NDK第三节课）

---
+++ 【07】NDK/（03）2021.2.28 静态开辟内存与动态开辟内存（NDK第三节课）/CProject/cmake-build-debug/CMakeFiles/CProject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CProject.dir/T1.c.obj"
+  "CMakeFiles/CProject.dir/T2.c.obj"
+  "CMakeFiles/CProject.dir/T3.c.obj"
+  "CMakeFiles/CProject.dir/T4.c.obj"
+  "CProject.exe"
+  "CProject.exe.manifest"
+  "CProject.pdb"
+  "libCProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
