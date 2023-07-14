cbc8cfe7592ae7e01744dd2ad1f10ab1e4cd2467
lyyyd <David.Jackson.Lyd@gmail.com>
2023-07-14 20:43:48 +0800

    （06）2021.3.7C语言游戏破解器与文件加解密（NDK第六节课）
    （06）2021.3.7C语言游戏破解器与文件加解密（NDK第六节课）

---
+++ 【07】NDK/（06）2021.3.7C语言游戏破解器与文件加解密（NDK第六节课）/ClionCProject/cmake-build-debug/CMakeFiles/ClionCProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClionCProject.dir/T11.c.obj"
+  "ClionCProject.exe"
+  "ClionCProject.exe.manifest"
+  "ClionCProject.pdb"
+  "libClionCProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ClionCProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
