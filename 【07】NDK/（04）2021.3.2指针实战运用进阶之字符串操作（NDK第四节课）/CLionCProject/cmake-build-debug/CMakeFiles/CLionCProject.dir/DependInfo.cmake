2e691905e88d08c826f99862a7ee1740124f07b0
lyyyd <David.Jackson.Lyd@gmail.com>
2023-07-14 20:40:42 +0800

    （04）2021.3.2指针实战运用进阶之字符串操作（NDK第四节课）
    （04）2021.3.2指针实战运用进阶之字符串操作（NDK第四节课）

---
+++ 【07】NDK/（04）2021.3.2指针实战运用进阶之字符串操作（NDK第四节课）/CLionCProject/cmake-build-debug/CMakeFiles/CLionCProject.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/T0.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/cmake-build-debug/CMakeFiles/CLionCProject.dir/T0.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/T1.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/cmake-build-debug/CMakeFiles/CLionCProject.dir/T1.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/T2.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/cmake-build-debug/CMakeFiles/CLionCProject.dir/T2.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/T3.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/cmake-build-debug/CMakeFiles/CLionCProject.dir/T3.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/T4.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/cmake-build-debug/CMakeFiles/CLionCProject.dir/T4.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/T5.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/cmake-build-debug/CMakeFiles/CLionCProject.dir/T5.c.obj"
+  "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/T6.c" "D:/jingtong_yunyon_chuangzhao_VIPALL_NDK_YoudaoNote/XiangXueNDKAllVIP/ndk04/project/CLionCProject/cmake-build-debug/CMakeFiles/CLionCProject.dir/T6.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 【07】NDK/（04）2021.3.2指针实战运用进阶之字符串操作（NDK第四节课）/CLionCProject/cmake-build-debug/CMakeFiles/CLionCProject.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CLionCProject.exe"
+  "CLionCProject.exe.manifest"
+  "CLionCProject.pdb"
+  "CMakeFiles/CLionCProject.dir/T0.c.obj"
+  "CMakeFiles/CLionCProject.dir/T1.c.obj"
+  "CMakeFiles/CLionCProject.dir/T2.c.obj"
+  "CMakeFiles/CLionCProject.dir/T3.c.obj"
+  "CMakeFiles/CLionCProject.dir/T4.c.obj"
+  "CMakeFiles/CLionCProject.dir/T5.c.obj"
+  "CMakeFiles/CLionCProject.dir/T6.c.obj"
+  "libCLionCProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CLionCProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
